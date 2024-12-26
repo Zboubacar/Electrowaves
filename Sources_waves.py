47847e9cce3e23644b698835d39df59452841a04
Zboubacar <theo.croizet@gmail.com>
2024-12-26 18:41:00 +0100

    26122024 Progress

---
+++ Sources_waves.py
@@ -0,0 +1,12 @@
+## Dispersion relation solving
+def Wave_disp_Stokes_kin_wout(k,h,g=9.81):
+    import numpy as np
+    w2=g*k*np.tanh(k*h)
+    w=np.sqrt(w2)
+
+    return w
+
+
+
+### Creation of Jonswap spectrum components using Stokes stheory
+
